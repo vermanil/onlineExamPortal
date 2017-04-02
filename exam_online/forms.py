fcfca7267b64e3730ecf517602276a0c94bd5453
vermanil <anilrajverma1996@gmail.com>
2017-04-03 01:29:13 +0530

    Add candidate registration and login

--- exam_online/forms.py
+++ exam_online/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 
-class candidateLoginForm(AuthenticationForm):
+class candidateLoginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=30,
                                widget=forms.TextInput(
                                    attrs={'class': 'border-gradient', 'name': 'username', 'placeholder': 'Username'}))
@@ -12,22 +12,22 @@ class candidateLoginForm(AuthenticationForm):
                                    attrs={'class': 'border-gradient', 'name': 'password', 'placeholder': 'Password'}))
 
 
-class RegistrationForm(AuthenticationForm):
+class RegistrationForm(forms.Form):
     first_name = forms.CharField(label='', required=True, widget=forms.TextInput(
-        attrs={'class': 'border-gradient', 'placeholder': 'First_Name'}))
+        attrs={'name': 'first_name','placeholder': 'First_Name'}))
     last_name = forms.CharField(label='',
-                                widget=forms.TextInput(attrs={'class': 'border-gradient', 'placeholder': 'Last_Name'}))
+                                widget=forms.TextInput(attrs={'name': 'last_name','placeholder': 'Last_Name'}))
     username = forms.CharField(label='',
-                               widget=forms.TextInput(attrs={'class': 'border-gradient', 'placeholder': 'UserName'}))
+                               widget=forms.TextInput(attrs={'name': 'username','placeholder': 'UserName'}))
     password = forms.CharField(label='',
-                               widget=forms.PasswordInput(attrs={'class': 'border-gradient', 'placeholder': 'Password'}))
+                               widget=forms.PasswordInput(attrs={'name': 'password','placeholder': 'Password'}))
     email = forms.EmailField(label='',
-                             widget=forms.EmailInput(attrs={'class': 'border-gradient', 'placeholder': 'Email'}))
+                             widget=forms.EmailInput(attrs={'name': 'email','placeholder': 'Email'}))
     # phone_number = forms.RegexField(regex=r'^\+?1?\d{9,15}$',
     #                                 error_message=(
     #                                 "Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."))
     #
-class InstituteRegistrationForm(AuthenticationForm):
+class InstituteRegistrationForm(forms.Form):
     InstituteName = forms.CharField(label='', required=True, widget=forms.TextInput(
         attrs={'class': 'border-gradient', 'placeholder': 'Institute Name'}))
     username = forms.CharField(label='',
@@ -37,7 +37,7 @@ class InstituteRegistrationForm(AuthenticationForm):
     email = forms.EmailField(label='',
                              widget=forms.EmailInput(attrs={'class': 'border-gradient', 'placeholder': 'Email'}))
 
-class InstituteLoginForm(AuthenticationForm):
+class InstituteLoginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=30,
                                widget=forms.TextInput(
                                    attrs={'class': 'border-gradient', 'name': 'username', 'placeholder': 'Username'}))
--- exam_online/views.py
+++ exam_online/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .forms import candidateLoginForm, RegistrationForm, InstituteLoginForm, InstituteRegistrationForm
 
 # Create your views here.
@@ -7,15 +9,47 @@ def index(request):
     return render(request, "home.html", {})
 
 def candidateLogin(request, name):
-    if name == "Candidate":
-        form = candidateLoginForm()
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            return HttpResponseRedirect("/")
+        else:
+            url = "/accounts/" + name + "/login"
+            return HttpResponseRedirect(url)
     else:
-        form = InstituteLoginForm()
-    return render(request, "Login.html", {'form': form, "name": name})
+        if name == "Candidate":
+            form = candidateLoginForm()
+        else:
+            form = InstituteLoginForm()
+        return render(request, "Login.html", {'form': form, "name": name})
 
 def candidateRegister(request,name):
-    if name=="Candidate":
-        form = RegistrationForm()
+    print(request.method)
+    print(name)
+    if request.method == 'POST':
+        if name=="Candidate":
+            print("hello")
+            form = RegistrationForm(request.POST)
+        else:
+            form = InstituteRegistrationForm(request.POST)
+        print(form.is_valid())
+        if form.is_valid():  # invoke .is_valid
+            user = User.objects.create_user(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
+                password=form.cleaned_data['password']
+            )
+            user.first_name = form.cleaned_data['first_name']
+            user.last_name = form.cleaned_data['last_name']
+            user.save()
+            url="/accounts/" + name +"/login"
+            print(url)
+        return HttpResponseRedirect(url)
     else:
-        form = InstituteRegistrationForm()
-    return render(request, "reg.html", {'form1': form, "name": name})
+        if name=="Candidate":
+            form = RegistrationForm()
+        else:
+            form = InstituteRegistrationForm()
+        return render(request, "reg.html", {'form': form, "name": name})
