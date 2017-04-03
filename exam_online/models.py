8ec92ae163411f85d0773c6db5c84867d938013f
vermanil <anilrajverma1996@gmail.com>
2017-04-03 16:08:28 +0530

    define model

--- exam_online/models.py
+++ exam_online/models.py
@@ -2,3 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+class exam_details(models.Model):
+    examCode = models.TextField()
+    examName = models.TextField()
+    totalTime = models.IntegerField()
+    totalMark = models.IntegerField()
+    createdby = models.TextField()
+
+class questionDetails(models.Model):
+    code = models.ForeignKey(exam_details.examCode)
+    question_id = models.IntegerField()
+    correctAnswer = models.TextField()
+
+class optionDetails(models.Model):
+    id = models.ForeignKey(questionDetails.question_id)
+    option1 = models.TextField()
+    option2 = models.TextField()
+    option3 = models.TextField()
+    option4 = models.TextField()
